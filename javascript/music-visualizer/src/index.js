e511382d9c7bf485d3de08f50a9f02cbac043304
Vladimir Zeifman <edoomik@gmail.com>
2017-08-26 06:35:08 +0300

    music-visualizer: Add empty canvas

--- javascript/music-visualizer/src/index.js
+++ javascript/music-visualizer/src/index.js
@@ -1,1 +1,37 @@
-console.log("hello world");
+import * as THREE from 'three';
+import OrbitControls from 'imports-loader?THREE=three!exports-loader?THREE.OrbitControls!three-extras/controls/OrbitControls';
+
+import './styles.css';
+
+
+const FOV = 45,
+    ASPECT = window.innerWidth / window.innerHeight,
+    NEAR = 0.1,
+    FAR = 1000;
+
+const scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+const renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.appendChild(renderer.domElement);
+
+/* eslint-disable no-unused-vars */
+const controls = new OrbitControls(camera, renderer.domElement);
+/* eslint-enable no-unused-vars */
+
+const GRID_SIZE = 100;
+const GRID_STEP = 10;
+const gridHelper = new THREE.GridHelper(GRID_SIZE, GRID_STEP);
+scene.add(gridHelper);
+
+camera.position.set(0, 5, 50);
+camera.lookAt(scene.position);
+
+
+function render() {
+    requestAnimationFrame(render);
+
+    renderer.render(scene, camera);
+}
+
+render();
--- javascript/music-visualizer/webpack.config.js
+++ javascript/music-visualizer/webpack.config.js
@@ -1,5 +1,6 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const webpack = require('webpack');
 
 
 module.exports = {
@@ -8,7 +9,15 @@ module.exports = {
     path: path.resolve(__dirname, 'dist'),
     filename: 'app.bundle.js'
   },
+  resolve: {
+      alias: {
+          'three-extras': path.resolve(__dirname, 'node_modules/three/examples/js/')
+        }
+    },
   plugins: [
+    new webpack.ProvidePlugin({
+        THREE: 'three'
+    }),
     new HtmlWebpackPlugin({
         title: 'Music Visualizer'
     })
